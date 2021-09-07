a29f99eec0693078fa633502ba891b8597606514
MasonBaek <82987683+MasonBaek@users.noreply.github.com>
2021-09-08 01:16:10 +0900

    Update BasketVO.java

--- G3/src/main/java/com/g3/domain/BasketVO.java
+++ G3/src/main/java/com/g3/domain/BasketVO.java
@@ -12,59 +12,59 @@ public class BasketVO {
 	private String b_p_price; // 상품가격
 	private Date b_date; // 장반구니담은날짜
 
-	int getB_num() {
+	public int getB_num() {
 		return b_num;
 	}
 
-	void setB_num(int b_num) {
+	public void setB_num(int b_num) {
 		this.b_num = b_num;
 	}
 
-	String getU_id() {
+	public String getU_id() {
 		return u_id;
 	}
 
-	void setU_id(String u_id) {
+	public void setU_id(String u_id) {
 		this.u_id = u_id;
 	}
 
-	int getB_p_num() {
+	public int getB_p_num() {
 		return b_p_num;
 	}
 
-	void setB_p_num(int b_p_num) {
+	public void setB_p_num(int b_p_num) {
 		this.b_p_num = b_p_num;
 	}
 
-	int getB_p_amount() {
+	public int getB_p_amount() {
 		return b_p_amount;
 	}
 
-	void setB_p_amount(int b_p_amount) {
+	public void setB_p_amount(int b_p_amount) {
 		this.b_p_amount = b_p_amount;
 	}
 
-	String getB_p_option() {
+	public String getB_p_option() {
 		return b_p_option;
 	}
 
-	void setB_p_option(String b_p_option) {
+	public void setB_p_option(String b_p_option) {
 		this.b_p_option = b_p_option;
 	}
 
-	String getB_p_price() {
+	public String getB_p_price() {
 		return b_p_price;
 	}
 
-	void setB_p_price(String b_p_price) {
+	public void setB_p_price(String b_p_price) {
 		this.b_p_price = b_p_price;
 	}
 
-	Date getB_date() {
+	public Date getB_date() {
 		return b_date;
 	}
 
-	void setB_date(Date b_date) {
+	public void setB_date(Date b_date) {
 		this.b_date = b_date;
 	}
 
