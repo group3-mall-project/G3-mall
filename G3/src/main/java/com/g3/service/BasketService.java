9bb4bb8f33f9837c251cc471b75d7306e5517fce
MasonBaek <82987683+MasonBaek@users.noreply.github.com>
2021-09-10 01:34:51 +0900

    Update BasketService.java

--- G3/src/main/java/com/g3/service/BasketService.java
+++ G3/src/main/java/com/g3/service/BasketService.java
@@ -1,6 +1,17 @@
 package com.g3.service;
 
+import java.util.List;
+
+import com.g3.domain.Ad_ProductVO;
+
 public interface BasketService {
 	
-	
+	//새 장바구니 추가
+	public void insertProductInBasket(Ad_ProductVO prod, String id);
+
+	//장바구니 목록 조회(VasketVO + p_image(g3_product))
+	public List basketList(String id);
+
+	//찜목록 조회 (
+	public List<Ad_ProductVO> getLikeList(String id);
 }
