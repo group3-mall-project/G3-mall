30f6ef313787ef64ef0112d71fecaca23c2fae12
Chang9601 <79137839+Chang9601@users.noreply.github.com>
2021-09-23 19:58:42 +0900

    Update BoardVO.java

--- G3/src/main/java/com/g3/domain/BoardVO.java
+++ G3/src/main/java/com/g3/domain/BoardVO.java
@@ -11,7 +11,6 @@ public class BoardVO {
 	private String bo_content;
 	private int bo_readcount;
 	private int bo_re_ref;
-	private int bo_re_lev;
 	private int bo_re_seq;
 	private String bo_updatedate;
 	private int bo_p_num;
